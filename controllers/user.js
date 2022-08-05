1f8352507d58b35b3c192a6f728289306c65974e
Hansel Reynoso <omarreynoso09@gmail.com>
2022-08-04 21:26:34 -0400

    updated users and routes

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,6 @@
 const bcrypt = require("bcrypt-nodejs");
 const User = require("../models/user");
+const jwt = require("../services/jwt");
 
 function signUp(req, res) {
   const user = new User();
@@ -40,6 +41,41 @@ function signUp(req, res) {
   }
 }
 
+function signIn(req, res) {
+  const params = req.body;
+  const email = params.email.toLowerCase();
+  const password = params.password;
+
+  User.findOne({ email }, (err, userStored) => {
+    if (err) {
+      res.status(500).send({ message: "Server Error." });
+    } else {
+      if (!userStored) {
+        res.status(404).send({ message: "User Not Found." });
+      } else {
+        bcrypt.compare(password, userStored.password, (err, check) => {
+          if (err) {
+            res.status(500).send({ message: "Server Error." });
+          } else if (!check) {
+            res.status(404).send({ message: "Incorrect Password." });
+          } else {
+            if (!userStored.active) {
+              res
+                .status(200)
+                .send({ code: 200, message: "The User Is Not Active." });
+            } else {
+              res.status(200).send({
+                accessToken: jwt.createAccessToken(userStored),
+                refreshToken: jwt.createRefreshToken(userStored),
+              });
+            }
+          }
+        });
+      }
+    }
+  });
+}
 module.exports = {
   signUp,
+  signIn,
 };
--- routers/user.js
+++ routers/user.js
@@ -4,5 +4,6 @@ const UserController = require("../controllers/user");
 const api = express.Router();
 
 api.post("/sign-up", UserController.signUp); //when this route happens is going to execute the usercontroller signup
+api.post("/sign-in", UserController.signIn); // to execute the signIn from the controllers
 
 module.exports = api;
