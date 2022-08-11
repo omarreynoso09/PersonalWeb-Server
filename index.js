b935204193aa3c36ab8cb7ea0da75d59eab21397
Hansel Reynoso <omarreynoso09@gmail.com>
2022-08-11 11:52:49 -0400

    first update to heroku

--- index.js
+++ index.js
@@ -3,18 +3,22 @@ const app = require("./app");
 const port = process.env.PORT || 3977;
 const { API_VERSION, IP_SERVER, PORT_DB } = require("./config");
 
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
+
 mongoose.connect(
-  `mongodb://${IP_SERVER}:${PORT_DB}/hanseldb`,
-  { useNewUrlParser: true, useUnifiedTopology: true },
+  process.env.MONGODB_URI,
+  { dbName: "hanseldb" },
+  // { useNewUrlParser: true, useUnifiedTopology: true },
   (err, res) => {
     if (err) {
       throw err;
     } else {
-      console.log("database connection success");
+      console.log("the connection is working.");
 
       app.listen(port, () => {
         console.log("######################");
-        console.log("###### HANSEL BACKEND ######");
+        console.log("###### API REST ######");
         console.log("######################");
         console.log(`http://${IP_SERVER}:${port}/api/${API_VERSION}/`);
       });
